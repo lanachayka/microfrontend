01e4a0fad4ffa78dd3b90c62b524c267500e7354
RuslanaPidsadiuk-brickredsystems <ruslana.pidsadiuk@brickredsystems.ca>
2025-08-22 13:31:01 +0300

    add navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,22 @@
 import React from "react";
-import {mount} from "marketing/MarketingApp";
+import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 export default () => {
-    const ref = React.useRef(null);
+  const ref = React.useRef(null);
+  const history = useHistory();
 
-    React.useEffect(() => {
-        mount(ref.current, {});
-    }, []);
+  React.useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const pathname = history.location.pathname;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    history.listen(onParentNavigate);
+  }, []);
 
-    return <div ref={ref} />;
-}
+  return <div ref={ref} />;
+};
